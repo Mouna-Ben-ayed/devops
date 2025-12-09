24585e8b82a6601bf7a21ac79329e7ea2a47513e
Ben Ayed Mouna <ben-ayed.mouna@enis.tn>
2025-12-09 11:43:47 +0000

    updates

--- post_consumer/main.py
+++ post_consumer/main.py
@@ -4,6 +4,7 @@ import json
 import os
 
 # Kafka
+# Ces variables sont lues depuis les arguments de la commande ou l'environnement (non montré ici)
 KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka-broker-service:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "posts")
 
@@ -16,6 +17,7 @@ consumer = KafkaConsumer(
 )
 
 # BigQuery
+# Définir explicitement l'emplacement du fichier Secret monté par Kubernetes
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/secrets/service-account.json"
 client = bigquery.Client()
 dataset_id = os.getenv("BQ_DATASET", "kafka_dataset")
